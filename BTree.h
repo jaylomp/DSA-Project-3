a4ecbc0fe0e62cf1c280ec3726143a3a0a344974
jaylomp <127219033+jaylomp@users.noreply.github.com>
2024-08-03 12:07:43 -0400

    Update BTree.h

--- BTree.h
+++ BTree.h
@@ -41,3 +41,26 @@ public:
 
     void buildFromDataset(const vector<CrimeRecord>& dataset);
 };
+
+
+
+// this is just a place holder we need to discuss what we want in the nodes
+struct CrimeRecord {
+    int crimeID;
+    string type;
+    string date;
+    string location;
+
+    // Comparison operators for BTree ordering by crimeID
+    bool operator<(const CrimeRecord& other) const {
+        return crimeID < other.crimeID;
+    }
+
+    bool operator>(const CrimeRecord& other) const {
+        return crimeID > other.crimeID;
+    }
+
+    bool operator==(const CrimeRecord& other) const {
+        return crimeID == other.crimeID;
+    }
+};
