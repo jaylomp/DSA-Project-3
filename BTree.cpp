9a30a5778aa5f09075aaec07050737b1a3b74e05
jaylomp <127219033+jaylomp@users.noreply.github.com>
2024-08-03 15:45:12 +0000

    :)

--- BTree.cpp
+++ BTree.cpp
@@ -1,2 +1,39 @@
 #include "BTree.h"
 
+
+BTreeNode::BTreeNode(int t, bool l) {
+    minDegree = t;
+    leaf = l;
+}
+
+void BTreeNode::traverse() {
+    int i;
+    for (int i = 0; i < keys.size(); i++) {
+        if (!leaf) {
+            children[i]->traverse();
+        }
+    }
+
+    if (!leaf) {
+        children[i]->traverse();
+    }
+}
+
+BTreeNode* BTreeNode::search(int key) {
+    int i = 0;
+
+    while (i < keys.size() && k > keys[i]) {
+        i++;
+    }
+
+    if (i < keys.size() && keys[i] == k) {
+        return this;
+    }
+
+    if (leaf) {
+        return nullptr;
+    }
+
+    return children[i]->search(k);
+} 
+
--- BTree.h
+++ BTree.h
@@ -1,5 +1,46 @@
 #pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class BTreeNode {
+    public:
+        bool leaf;
+        vector<int> keys;
+        vector<BTreeNode*> children;
+        int minDegree;
+
+        BTreeNode(int t, bool l);
+
+        void insertNotfull(int k);
+
+        void splitChild(int x, BTreeNode* y);
+
+        void traverse();
+
+        BTreeNode* search(int key);
+
+        friend class BTree;
+
+
+
+};
+
+
 
 class BTree {
+public:
+    BTreeNode* root;
+    int minDegree;
+
+    BTree(int t) {root = nullptr, minDegree = t;}
+
+    void traverse() {if (root != nullptr) root->traverse();}
+
+    BTreeNode* search(int key) {
+        return (root == nullptr) ? nullptr : root->search(key);
+    }
+
+    void insert (int k);
 
 };
