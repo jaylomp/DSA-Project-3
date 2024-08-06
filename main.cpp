127bfce7eb3fb6fab3e09bf44cbcbd13c02458d7
jaylomp <127219033+jaylomp@users.noreply.github.com>
2024-08-06 19:03:39 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,22 @@
 int main() {
     std::vector<CrimeRecord> records = readCSV("FloridaConvictions_00000.csv", 10000000);
     HashTable table;
-    // BTree tree;
+    // HashTable;
     for (auto& i : records) {
         i.county = normalizeCountyName(i.county);
         i.finalLevel = normalizeFinalLevel(i.finalLevel);
         table.insert(i);
         // tree.insert(i);
     }
+
+
+    //BTree
+
+    BTree tree(32); // minDegree
+    tree.buildFromDataset(records);
+    
+    //string countyToSearch = "Pinellas County";
+    //tree.timeCountFeloniesInCounty(countyToSearch);
+    
+    
 }
