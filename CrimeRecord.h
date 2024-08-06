bd16b56875e3889e142d6a731b1b2851c18841da
jaylomp <127219033+jaylomp@users.noreply.github.com>
2024-08-06 12:20:42 -0400

    Update CrimeRecord.h

--- CrimeRecord.h
+++ CrimeRecord.h
@@ -2,24 +2,25 @@
 #include <string>
 using namespace std;
 
-
-// this is just a place holder we need to discuss what we want in the nodes
-struct CrimeRecord {
+class CrimeRecord {
+public:
     int crimeID;
-    string type;
-    string date;
+    string crimeType;
     string location;
+    string date;
 
-    // Comparison operators for BTree ordering by crimeID
+    // Default constructor
+    CrimeRecord() : crimeID(0), crimeType("Unknown"), location("Unknown"), date("Unknown") {}
+
+    // Parameterized constructor
+    CrimeRecord(int id, const string& type, const string& loc, const string& dt)
+        : crimeID(id), crimeType(type), location(loc), date(dt) {}
+
+    // Overload comparison operators for CrimeRecord
     bool operator<(const CrimeRecord& other) const {
         return crimeID < other.crimeID;
     }
-
     bool operator>(const CrimeRecord& other) const {
         return crimeID > other.crimeID;
     }
-
-    bool operator==(const CrimeRecord& other) const {
-        return crimeID == other.crimeID;
-    }
-}; 
+};
